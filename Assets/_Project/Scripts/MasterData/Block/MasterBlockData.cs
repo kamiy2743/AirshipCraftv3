c0f8572c3c673daed671261ac42cbe5c8981e64e
kamiy2743 <kamiy2743@gmail.com>
2022-08-30 13:58:42 +0900

    フォルダ整理

--- Assets/_Project/Scripts/MasterData/Block/MasterBlockData.cs
+++ Assets/_Project/Scripts/MasterData/Block/MasterBlockData.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using MasterData.Util;
 
 namespace MasterData.Block
 {
--- Assets/_Project/Scripts/MasterData/Util/CubeMesh.cs
+++ Assets/_Project/Scripts/MasterData/Util/CubeMesh.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-namespace MasterData
+namespace MasterData.Util
 {
     public static class CubeMesh
     {
