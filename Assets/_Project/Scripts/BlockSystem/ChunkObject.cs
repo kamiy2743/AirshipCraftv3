c15bb0e35072292ee9dd63207931cbf79fefee13
kamiy2743 <kamiy2743@gmail.com>
2022-09-01 13:59:05 +0900

    チャンクメッシュの生成処理用クラス作成

--- Assets/_Project/Scripts/BlockSystem/ChunkObject.cs
+++ Assets/_Project/Scripts/BlockSystem/ChunkObject.cs
@@ -9,8 +9,6 @@ namespace BlockSystem
         private MeshFilter meshFilter;
         private MeshRenderer meshRenderer;
 
-        private int BlockSide => WorldSettings.LocalBlockSide;
-
         internal void Initial(ChunkData chunkData)
         {
             meshFilter = GetComponent<MeshFilter>();
@@ -18,30 +16,7 @@ namespace BlockSystem
 
             ChunkData = chunkData;
 
-            var meshCombiner = new MeshCombiner();
-
-            // メッシュをバッチングする
-            for (int y = 0; y < BlockSide; y++)
-            {
-                for (int z = 0; z < BlockSide; z++)
-                {
-                    for (int x = 0; x < BlockSide; x++)
-                    {
-                        var blockData = chunkData.GetBlockData(new LocalCoordinate(x, y, z));
-
-                        // 他のブロックに接している面を計算
-                        var contactOtherBlockSurfaces = ContactOtherBlockSolver.Instance.GetContactOtherBlockSurfaces(blockData.BlockCoordinate);
-                        blockData.SetContactOtherBlockSurfaces(contactOtherBlockSurfaces);
-
-                        // 空気に接していない = 見えないので描画しない
-                        if (!blockData.IsContactAir) continue;
-
-                        meshCombiner.AddBlock(blockData);
-                    }
-                }
-            }
-
-            meshFilter.mesh = meshCombiner.Combine();
+            meshFilter.mesh = ChunkMeshCreator.CreateMesh(chunkData);
         }
     }
 }
