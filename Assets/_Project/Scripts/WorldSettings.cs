1f21a9a4b39ac95ad1346854609cf09d50a9f3d8
kamiy2743 <kamiy2743@gmail.com>
2022-09-01 13:16:32 +0900

    設定変更

--- Assets/_Project/Scripts/WorldSettings.cs
+++ Assets/_Project/Scripts/WorldSettings.cs
@@ -3,12 +3,12 @@ namespace WorldSystem
     public static class WorldSettings
     {
         /// <summary>ワールドの横の長さ(チャンク換算)</summary>
-        public const int WorldChunkSideXZ = 7;
+        public const int WorldChunkSideXZ = LoadChunkRadius * 2 + 1;
         /// <summary>ワールドの縦の長さ(チャンク換算)</summary>
-        public const int WorldChunkSideY = 7;
+        public const int WorldChunkSideY = 4;
 
         /// <summary>プレイヤーの周りの読み込み距離(チャンク換算)</summary>
-        public const int LoadChunkRadius = 3;
+        public const int LoadChunkRadius = 16;
 
         /// <summary>チャンク内を満たすブロックの立方体の一辺の長さ</summary>
         public const int LocalBlockSide = 16;
