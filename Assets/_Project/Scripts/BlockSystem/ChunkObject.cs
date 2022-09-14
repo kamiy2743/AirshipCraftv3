7ddbe666b7f837957ab1a3b7a162ce2a2afb75d9
kamiy2743 <kamiy2743@gmail.com>
2022-09-14 14:16:27 +0900

    チャンクにコライダー追加 今のところMeshColliderでもそんなに重くない

--- Assets/_Project/Scripts/BlockSystem/ChunkObject.cs
+++ Assets/_Project/Scripts/BlockSystem/ChunkObject.cs
@@ -6,16 +6,27 @@ namespace BlockSystem
     {
         [SerializeField] private MeshFilter meshFilter;
         [SerializeField] private MeshRenderer meshRenderer;
+        [SerializeField] private MeshCollider meshCollider;
 
         internal void SetMesh(ChunkMeshData meshData)
         {
             var mesh = meshFilter.mesh;
             mesh.Clear();
 
+            if (meshData.IsEmpty)
+            {
+                meshCollider.sharedMesh = null;
+                meshCollider.enabled = false;
+                return;
+            }
+
             mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
             mesh.SetVertices(meshData.Vertices);
             mesh.SetTriangles(meshData.Triangles, 0);
             mesh.RecalculateNormals();
+
+            meshCollider.sharedMesh = mesh;
+            meshCollider.enabled = true;
         }
     }
 }
