ed64c656417f68d6e8130bf28ec4360b777477e8
kamiy2743 <kamiy2743@gmail.com>
2022-09-19 20:09:02 +0900

    プレイヤーをrigidbodyで動かす

--- Assets/_Project/Scripts/Input/InputProvider.cs
+++ Assets/_Project/Scripts/Input/InputProvider.cs
@@ -16,12 +16,23 @@ namespace Input
             return inputActions;
         }
 
+        public static Vector3 Move()
+        {
+            var inputVector = InputActions.Player.Move.ReadValue<Vector2>();
+            return new Vector3(inputVector.x, 0, inputVector.y).normalized;
+        }
+
         public static Vector3 DebugMove()
         {
             var horizontal = InputActions.Player.DebugMoveHorizontal.ReadValue<Vector2>();
             var vertical = InputActions.Player.DebugMoveVertical.ReadValue<float>();
 
             return new Vector3(horizontal.x, vertical, horizontal.y).normalized;
+        }
+
+        public static bool Jump()
+        {
+            return InputActions.Player.Jump.triggered;
         }
     }
 }
--- Assets/_Project/Scripts/Player/PlayerMover.cs
+++ Assets/_Project/Scripts/Player/PlayerMover.cs
@@ -9,11 +9,43 @@ namespace Player
     internal class PlayerMover : MonoBehaviour
     {
         [SerializeField] private PlayerCamera playerCamera;
+        [Space(20)]
         [SerializeField] private float moveSpeed;
+        [SerializeField] private float jumpForce;
+        [SerializeField] private float gravity;
+        [Space(20)]
+        [SerializeField] private float debugMoveSpeed;
+
+        private Rigidbody rigidbody;
+
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+        }
 
         private void Update()
         {
-            transform.position += playerCamera.HorizontalRotation * (InputProvider.DebugMove() * moveSpeed * Time.deltaTime);
+            Move();
+        }
+
+        private void Move()
+        {
+            var moveVector = InputProvider.Move() * moveSpeed;
+            moveVector.y = rigidbody.velocity.y;
+            rigidbody.velocity = playerCamera.HorizontalRotation * moveVector;
+
+            if (InputProvider.Jump())
+            {
+                Debug.Log("jump");
+                rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+
+            rigidbody.AddForce(Vector3.down * gravity);
+        }
+
+        private void DebugMove()
+        {
+            transform.position += playerCamera.HorizontalRotation * (InputProvider.DebugMove() * debugMoveSpeed * Time.deltaTime);
         }
     }
 }
