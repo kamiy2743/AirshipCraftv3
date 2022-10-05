b94fabd02cae9341a6f5e87e662eec58d983320e
kamiy2743 <kamiy2743@gmail.com>
2022-10-05 12:14:28 +0900

    DebugMoveをMoveに統合

--- Assets/_Project/Scripts/Input/InputProvider.cs
+++ Assets/_Project/Scripts/Input/InputProvider.cs
@@ -16,14 +16,14 @@ namespace Input
 
         public static Vector3 Move()
         {
-            var inputVector = InputActions.Player.Move.ReadValue<Vector2>();
+            var inputVector = InputActions.Player.HorizontalMove.ReadValue<Vector2>();
             return new Vector3(inputVector.x, 0, inputVector.y).normalized;
         }
 
         public static Vector3 DebugMove()
         {
-            var horizontal = InputActions.Player.DebugMoveHorizontal.ReadValue<Vector2>();
-            var vertical = InputActions.Player.DebugMoveVertical.ReadValue<float>();
+            var horizontal = InputActions.Player.HorizontalMove.ReadValue<Vector2>();
+            var vertical = InputActions.Player.VerticalMove.ReadValue<float>();
 
             return new Vector3(horizontal.x, vertical, horizontal.y).normalized;
         }
