8efc14efc42578f66ca45008c6c8b9fbd1787ee2
kamiy2743 <kamiy2743@gmail.com>
2023-01-19 15:07:43 +0900

    描画面更新実行前の状態もテスト

--- Assets/_Project/Scripts/New/EditorTests/UpdateRenderingSurfaceTest.cs
+++ Assets/_Project/Scripts/New/EditorTests/UpdateRenderingSurfaceTest.cs
@@ -26,19 +26,32 @@ public class UpdateRenderingSurfaceTest
         var targetRelativeCoordinate = RelativeCoordinate.Parse(targetCoordinate);
         var targetChunkGridCoordinate = ChunkGridCoordinate.Parse(targetCoordinate);
 
-        var targetBlock = chunkProvider.GetChunk(targetChunkGridCoordinate).GetBlock(targetRelativeCoordinate);
-        updateBlockRenderingSurfaceService.UpdateBlockRenderingSurface(targetCoordinate, targetBlock.blockTypeID);
-
         var renderingSurface = renderingSurfaceProvider.GetRenderingSurface(targetChunkGridCoordinate);
 
         var target = renderingSurface.GetBlockRenderingSurface(targetRelativeCoordinate);
-
         var right = renderingSurface.GetBlockRenderingSurface(targetRelativeCoordinate.Add(x: 1));
         var left = renderingSurface.GetBlockRenderingSurface(targetRelativeCoordinate.Add(x: -1));
         var top = renderingSurface.GetBlockRenderingSurface(targetRelativeCoordinate.Add(y: 1));
         var bottom = renderingSurface.GetBlockRenderingSurface(targetRelativeCoordinate.Add(y: -1));
         var forward = renderingSurface.GetBlockRenderingSurface(targetRelativeCoordinate.Add(z: 1));
         var back = renderingSurface.GetBlockRenderingSurface(targetRelativeCoordinate.Add(z: -1));
+
+        Assert.AreEqual(false, target.Contains(Surface.Left));
+        Assert.AreEqual(false, target.Contains(Surface.Right));
+        Assert.AreEqual(false, target.Contains(Surface.Top));
+        Assert.AreEqual(false, target.Contains(Surface.Bottom));
+        Assert.AreEqual(false, target.Contains(Surface.Forward));
+        Assert.AreEqual(false, target.Contains(Surface.Back));
+
+        Assert.AreEqual(false, right.Contains(Surface.Left));
+        Assert.AreEqual(false, left.Contains(Surface.Right));
+        Assert.AreEqual(false, top.Contains(Surface.Bottom));
+        Assert.AreEqual(false, bottom.Contains(Surface.Top));
+        Assert.AreEqual(false, forward.Contains(Surface.Back));
+        Assert.AreEqual(false, back.Contains(Surface.Forward));
+
+        var targetBlock = chunkProvider.GetChunk(targetChunkGridCoordinate).GetBlock(targetRelativeCoordinate);
+        updateBlockRenderingSurfaceService.UpdateBlockRenderingSurface(targetCoordinate, targetBlock.blockTypeID);
 
         Assert.AreEqual(false, target.Contains(Surface.Left));
         Assert.AreEqual(false, target.Contains(Surface.Right));
