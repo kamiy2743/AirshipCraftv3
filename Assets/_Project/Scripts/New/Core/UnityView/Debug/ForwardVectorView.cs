3f76c96d95a29e1aeda0ac38b64e993811b2ba72
kamiy2743 <kamiy2743@gmail.com>
2023-01-29 21:56:10 +0900

    カメラの方向をデバッグ表示

---
+++ Assets/_Project/Scripts/New/Core/UnityView/Debug/ForwardVectorView.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityView.Players;
+using Zenject;
+using TMPro;
+
+namespace UnityView.Debug
+{
+    internal class ForwardVectorView : MonoBehaviour
+    {
+        [Inject] private PlayerCamera playerCamera;
+
+        [SerializeField] private TextMeshProUGUI text;
+
+        private void Update()
+        {
+            var xColor = "black";
+            if (playerCamera.Forward.x >= 0.5f) xColor = "red";
+            if (playerCamera.Forward.x <= -0.5f) xColor = "blue";
+            var x = $"<color=\"{xColor}\">{playerCamera.Forward.x.ToString("F1")}</color>";
+
+            var yColor = "black";
+            if (playerCamera.Forward.y >= 0.5f) yColor = "red";
+            if (playerCamera.Forward.y <= -0.5f) yColor = "blue";
+            var y = $"<color=\"{yColor}\">{playerCamera.Forward.y.ToString("F1")}</color>";
+
+            var zColor = "black";
+            if (playerCamera.Forward.z >= 0.5f) zColor = "red";
+            if (playerCamera.Forward.z <= -0.5f) zColor = "blue";
+            var z = $"<color=\"{zColor}\">{playerCamera.Forward.z.ToString("F1")}</color>";
+
+            text.text = $"Forward: ({x}, {y}, {z})";
+        }
+    }
+}
--- Assets/_Project/Scripts/New/Core/UnityView/Players/PlayerCamera.cs
+++ Assets/_Project/Scripts/New/Core/UnityView/Players/PlayerCamera.cs
@@ -6,7 +6,10 @@ namespace UnityView.Players
     internal class PlayerCamera
     {
         private CinemachinePOV cinemachinePOV;
+
         internal Quaternion HorizontalRotation => Quaternion.Euler(0, cinemachinePOV.m_HorizontalAxis.Value, 0);
+        internal Quaternion VerticalRotation => Quaternion.Euler(cinemachinePOV.m_VerticalAxis.Value, 0, 0);
+        internal Vector3 Forward => HorizontalRotation * VerticalRotation * Vector3.forward;
 
         internal PlayerCamera(CinemachineVirtualCamera vcam)
         {
