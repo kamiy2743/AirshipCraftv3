cd6181527e1b4b077053a5fd6ccecee591353a55
kamiy2743 <kamiy2743@gmail.com>
2023-01-29 14:20:00 +0900

    仮の実装ではあるけど、今後のデバッグ効率のために軽量化

--- Assets/_Project/Scripts/New/Core/UnityView/ChunkRendering/Mesh/BlockMeshProvider.cs
+++ Assets/_Project/Scripts/New/Core/UnityView/ChunkRendering/Mesh/BlockMeshProvider.cs
@@ -5,17 +5,27 @@ namespace UnityView.ChunkRendering.Mesh
 {
     internal class BlockMeshProvider : IBlockMeshProvider
     {
-        public BlockMesh GetBlockMesh(BlockTypeID blockTypeID)
+        private BlockMesh air;
+        private BlockMesh other;
+
+        internal BlockMeshProvider()
         {
-            if (blockTypeID == BlockTypeID.Air)
-            {
-                return new BlockMesh(new Vector3[0], new int[0], new Vector2[0]);
-            }
+            air = new BlockMesh(new Vector3[0], new int[0], new Vector2[0]);
 
             var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             var cubeMesh = cube.GetComponent<MeshFilter>().mesh;
             MonoBehaviour.Destroy(cube);
-            return new BlockMesh(cubeMesh.vertices, cubeMesh.triangles, cubeMesh.uv);
+            other = new BlockMesh(cubeMesh.vertices, cubeMesh.triangles, cubeMesh.uv);
+        }
+
+        public BlockMesh GetBlockMesh(BlockTypeID blockTypeID)
+        {
+            if (blockTypeID == BlockTypeID.Air)
+            {
+                return air;
+            }
+
+            return other;
         }
     }
 }
