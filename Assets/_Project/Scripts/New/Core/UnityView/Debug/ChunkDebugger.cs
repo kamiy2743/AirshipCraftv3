ceada282b2c602701fab78594d4a93ec6413ebbc
kamiy2743 <kamiy2743@gmail.com>
2023-01-29 21:59:06 +0900

    ChunkDebuggerをOldから移植

---
+++ Assets/_Project/Scripts/New/Core/UnityView/Debug/ChunkDebugger.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using Unity.Mathematics;
+using Domain.Chunks;
+
+namespace UnityView.Debug
+{
+    internal class ChunkDebugger : MonoBehaviour
+    {
+        [SerializeField] private Transform player;
+        [Space(20)]
+        [SerializeField] private bool drawGizmo = true;
+        [SerializeField, Range(0, 16)] private int drawRadius;
+        [SerializeField] private bool simpleDraw = false;
+
+        private void OnDrawGizmos()
+        {
+            if (!drawGizmo) return;
+            DrawLoadedChunk();
+        }
+
+        private void DrawLoadedChunk()
+        {
+            var pc = GetPlayerChunk(player.position);
+            var size = Vector3.one * Chunk.BlockSide;
+
+            if (simpleDraw)
+            {
+                Gizmos.color = GetRadiusColor(drawRadius);
+                Gizmos.DrawWireCube(pc * Chunk.BlockSide + size * 0.5f, Vector3.one * (drawRadius * 2 + 1) * Chunk.BlockSide);
+                Gizmos.DrawWireCube(pc * Chunk.BlockSide + size * 0.5f, Vector3.one * ((drawRadius - 1) * 2 + 1) * Chunk.BlockSide);
+                return;
+            }
+
+            void DrawWireCube(Vector3 chunkDelta = default, int radius = 0)
+            {
+                Gizmos.color = GetRadiusColor(radius);
+                Gizmos.DrawWireCube((pc + chunkDelta) * Chunk.BlockSide + (size * 0.5f), size);
+            }
+
+            for (int r = drawRadius; r > 0; r--)
+            {
+                for (int x = -r; x <= r; x++)
+                {
+                    for (int y = -r; y <= r; y++)
+                    {
+                        for (int z = -r; z <= r; z++)
+                        {
+                            DrawWireCube(new Vector3(x, y, z), r);
+                        }
+                    }
+                }
+            }
+
+            DrawWireCube();
+        }
+
+        private Vector3 GetPlayerChunk(Vector3 playerPosition)
+        {
+            return new Vector3(
+                (int)math.floor(playerPosition.x) >> Chunk.BlockSideShift,
+                (int)math.floor(playerPosition.y) >> Chunk.BlockSideShift,
+                (int)math.floor(playerPosition.z) >> Chunk.BlockSideShift)
+           ;
+        }
+
+        private Color GetRadiusColor(int radius)
+        {
+            var h = (1f / 8f) * Mathf.Repeat(radius, 8);
+            return Color.HSVToRGB(h, 1, 1);
+        }
+    }
+}
