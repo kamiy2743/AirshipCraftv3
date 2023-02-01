12a2c5a04f16230f569e9be2fb359dfa1d9f9a0d
kamiy2743 <kamiy2743@gmail.com>
2023-02-01 15:13:11 +0900

    チャンク描画更新のバグ修正

--- Assets/_Project/Scripts/New/Core/UnityView/ChunkRender/Surfaces/UpdatedChunkSurfaceCalculator.cs
+++ Assets/_Project/Scripts/New/Core/UnityView/ChunkRender/Surfaces/UpdatedChunkSurfaceCalculator.cs
@@ -17,62 +17,68 @@ namespace UnityView.ChunkRender
 
         internal IEnumerable<ChunkSurface> Calculate(BlockGridCoordinate updateCoordinate)
         {
-            var updatedChunkSurfaces = new Dictionary<ChunkGridCoordinate, ChunkSurface>();
+            var updatedChunkSurfaces = new Dictionary<ChunkGridCoordinate, ChunkSurface>(5);
             var targetBlockSurface = new BlockSurface();
             var targetChunkGridCoordinate = ChunkGridCoordinate.Parse(updateCoordinate);
             var targetRelativeCoordinate = RelativeCoordinate.Parse(updateCoordinate);
             var targetBlockTypeID = chunkProvider.GetChunk(targetChunkGridCoordinate).GetBlock(targetRelativeCoordinate).blockTypeID;
 
-            foreach (var surface in DirectionExt.Array)
+            foreach (var direction in DirectionExt.Array)
             {
                 // 隣接しているブロックの座標を取得
-                if (!updateCoordinate.TryAdd(surface.ToInt3(), out var adjacentCoordinate))
+                if (!updateCoordinate.TryAdd(direction.ToInt3(), out var adjacentCoordinate))
                 {
                     continue;
                 }
 
                 var adjacentChunkGridCoordinate = ChunkGridCoordinate.Parse(adjacentCoordinate);
                 var adjacentRelativeCoordinate = RelativeCoordinate.Parse(adjacentCoordinate);
-                var adjacentBlock = chunkProvider.GetChunk(adjacentChunkGridCoordinate).GetBlock(adjacentRelativeCoordinate);
 
                 if (targetBlockTypeID != BlockTypeID.Air)
                 {
                     // 対象ブロックの描画面に追加
+                    var adjacentBlock = chunkProvider.GetChunk(adjacentChunkGridCoordinate).GetBlock(adjacentRelativeCoordinate);
                     if (adjacentBlock.blockTypeID == BlockTypeID.Air)
                     {
-                        targetBlockSurface += surface;
+                        targetBlockSurface += direction;
                     }
                 }
 
-                // 隣接ブロックの描画面を更新
-                var adjacentRenderingSurface = chunkSurfaceProvider.GetChunkSurface(adjacentChunkGridCoordinate);
-                var currentSurface = adjacentRenderingSurface.GetBlockSurface(adjacentRelativeCoordinate);
-                var targetSurface = surface.Flip();
+                if (!updatedChunkSurfaces.TryGetValue(adjacentChunkGridCoordinate, out var adjacentChunkSurface))
+                {
+                    adjacentChunkSurface = chunkSurfaceProvider.GetChunkSurface(adjacentChunkGridCoordinate);
+                }
+
+                var currentSurface = adjacentChunkSurface.GetBlockSurface(adjacentRelativeCoordinate);
+                var targetDirection = direction.Flip();
 
                 if (targetBlockTypeID == BlockTypeID.Air)
                 {
-                    if (!currentSurface.Contains(targetSurface))
+                    if (!currentSurface.Contains(targetDirection))
                     {
-                        adjacentRenderingSurface.SetBlockSurfaceDirectly(adjacentRelativeCoordinate, currentSurface + targetSurface);
-                        updatedChunkSurfaces[adjacentChunkGridCoordinate] = adjacentRenderingSurface;
+                        adjacentChunkSurface.SetBlockSurfaceDirectly(adjacentRelativeCoordinate, currentSurface + targetDirection);
+                        updatedChunkSurfaces[adjacentChunkGridCoordinate] = adjacentChunkSurface;
                     }
                 }
                 else
                 {
-                    if (currentSurface.Contains(targetSurface))
+                    if (currentSurface.Contains(targetDirection))
                     {
-                        adjacentRenderingSurface.SetBlockSurfaceDirectly(adjacentRelativeCoordinate, currentSurface - targetSurface);
-                        updatedChunkSurfaces[adjacentChunkGridCoordinate] = adjacentRenderingSurface;
+                        adjacentChunkSurface.SetBlockSurfaceDirectly(adjacentRelativeCoordinate, currentSurface - targetDirection);
+                        updatedChunkSurfaces[adjacentChunkGridCoordinate] = adjacentChunkSurface;
                     }
                 }
             }
 
             // 対象ブロックの描画面を更新
             {
-                var renderingSurface = chunkSurfaceProvider.GetChunkSurface(targetChunkGridCoordinate);
-                renderingSurface.SetBlockSurfaceDirectly(targetRelativeCoordinate, targetBlockSurface);
+                if (!updatedChunkSurfaces.TryGetValue(targetChunkGridCoordinate, out var targetChunkSurface))
+                {
+                    targetChunkSurface = chunkSurfaceProvider.GetChunkSurface(targetChunkGridCoordinate);
+                }
 
-                updatedChunkSurfaces[targetChunkGridCoordinate] = renderingSurface;
+                targetChunkSurface.SetBlockSurfaceDirectly(targetRelativeCoordinate, targetBlockSurface);
+                updatedChunkSurfaces[targetChunkGridCoordinate] = targetChunkSurface;
             }
 
             return updatedChunkSurfaces.Values;
