9fea37607c342923f625162ced5e0ff53bd747fe
kamiy2743 <kamiy2743@gmail.com>
2023-03-22 20:08:36 +0900

    アイテムと個数を表すクラス追加

---
+++ Assets/Scripts/ACv3/Domain/Items/Core/StackItem.cs
@@ -0,0 +1,36 @@
+namespace ACv3.Domain.Items
+{
+    public record StackItem
+    {
+        readonly IItem item;
+        public readonly Amount Amount;
+        public ItemId ItemId => item.ItemId;
+
+        public static StackItem Empty => new(new EmptyItem(), Amount.Empty());
+
+        public StackItem(IItem item, Amount amount)
+        {
+            this.item = item;
+            Amount = amount;
+        }
+
+        public (StackItem primaryItem, StackItem subItem) Merge(StackItem other)
+        {
+            if (ItemId != other.ItemId)
+            {
+                return (this, other);
+            }
+            
+            var sumAmount = Amount + other.Amount;
+            
+            if (sumAmount <= item.MaxAmount)
+            {
+                return (new StackItem(item, sumAmount), Empty);
+            }
+
+            var primaryItem = new StackItem(item, item.MaxAmount);
+            var subItem = new StackItem(item, sumAmount - item.MaxAmount);
+            return (primaryItem, subItem);
+        }
+    }
+}
