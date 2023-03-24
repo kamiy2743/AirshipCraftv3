1d65541e605d6b32e5aa9eb52e286ef526a5dc50
kamiy2743 <kamiy2743@gmail.com>
2023-03-24 21:47:36 +0900

    EmptyのIdが空なのは治安悪そう

--- Assets/Scripts/ACv3/Domain/Items/Core/ItemId.cs
+++ Assets/Scripts/ACv3/Domain/Items/Core/ItemId.cs
@@ -3,16 +3,17 @@ namespace ACv3.Domain.Items
     public record ItemId
     {
         readonly string id;
+        const string EmptyId = "Empty";
 
         ItemId(string id)
         {
             this.id = id;
         }
 
-        public string RawString() => id;
+        public string RawString() => id == EmptyId ? "" : id;
         public override string ToString() => $"ItemId: {id}";
 
-        public static ItemId Empty => new("");
+        public static ItemId Empty => new(EmptyId);
         public static ItemId Dirt => new("Dirt");
         public static ItemId Stone => new("Stone");
     }
